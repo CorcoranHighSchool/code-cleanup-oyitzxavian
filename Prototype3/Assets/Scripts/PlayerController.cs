4a70d7dd9f231c7efa8cdb389b5de52ae411c606
oyitzxavian <142622316+oyitzxavian@users.noreply.github.com>
2024-04-09 11:55:03 -0700

    Removed unused Libraries. Serialized Public Properties. Private set for public properties used in other classes. Made strings Const. PlayerController.cs

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,27 +7,33 @@ public class PlayerController : MonoBehaviour
     //Jump force
     private float jumpForce = 15.0f;
     //Gravity Modifier
-    public float gravityModifier;
+    [SerializeField] private float gravityModifier;
     //Are we on the ground?
     private bool isOnGround = true;
     //Is the Game Over
-    public bool gameOver = false;
+    public bool gameOver {get; private set;};
 
     //Player Animator
     private Animator playerAnim;
 
     //ParticleSystem explosion
-    public ParticleSystem explositionParticle;
+    [SerializeField] private ParticleSystem explositionParticle;
     //ParticleSystem dirt
-    public ParticleSystem dirtParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
 
     //Jump sound
-    public AudioClip jumpSound;
+    [SerializeField] private AudioClip jumpSound;
     //Crash sound
-    public AudioClip crashSound;
+    [SerializeField] private AudioClip crashSound;
     //Player Audio
-    public AudioSource playerAudio;
-
+    [SerializeField] private AudioSource playerAudio;
+    //Const strings
+    private const string gameOverString = "Game Over!"; 
+    private const string obstacle = "Obstacle";
+    private const string ground = "Ground";
+    private const string deathB = "Death_b";
+    private const string deathType = "DeathType_int";
+    private const string jumpTrigger = "Jump_trig";
     // Start is called before the first frame update
     void Start()
     {
@@ -51,7 +55,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             //trigger the jump animation
-            playerAnim.SetTrigger("Jump_trig");
+            playerAnim.SetTrigger(jumpTrig);
             isOnGround = false;
             playerAudio.PlayOneShot(jumpSound, 1.0f);
             dirtParticle.Stop();
@@ -61,21 +65,21 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag(ground))
         {
             dirtParticle.Play();
             isOnGround = true;
         }
-        else if (collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag(obstacle))
         {
             explositionParticle.Play();
             dirtParticle.Stop();
             playerAudio.PlayOneShot(crashSound, 1.0f);
 
             gameOver = true;
-            Debug.Log("Game Over!");
-            playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1);
+            Debug.Log(gameOverString);
+            playerAnim.SetBool(deathB, true);
+            playerAnim.SetInteger(deathType, 1);
         }
     }
 }
