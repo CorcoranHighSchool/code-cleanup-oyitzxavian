3f3c8407257b12d01aa04dbb87bd1db47a7dce69
oyitzxavian <142622316+oyitzxavian@users.noreply.github.com>
2024-04-11 13:27:45 -0700

    removed unused libraries. Convert String to Constants. RepeatBackground.cs

--- Prototype3/Assets/Scripts/RepeatBackground.cs
+++ Prototype3/Assets/Scripts/RepeatBackground.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
-{
+{ 
     //starting Position
     private Vector3 startPos;
     //repeat width
     private float repeatWidth;
-
+    private const string gameOverString = "Game Over!";
     // Start is called before the first frame update
     void Start()
     {
